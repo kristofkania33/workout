0aa9887cc1dbf8ceea309c46cd4c77c15d0e1276
kristofkania33 <zyhkana25@wp.pl>
2017-06-29 14:05:14 +0200

    better data

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,51 @@
-var add = document.getElementById('addElement'),
-    train = document.getElementById('train'),
-    time = document.getElementById('time'),
+var addWorkout = document.getElementById('addWorkout'),
+    workout_name = document.getElementById('workout_name'),
+    workout_time = document.getElementById('workout_time'),
     excercise = document.getElementById('excercise');
+
+var excercise_set = [{name: "brzuszki", time: "5"}];
   
 
-add.addEventListener('click', function() {
-        var excercise1 = document.createElement('tr');
-        excercise1.innerHTML = 'Trening: ' + train + ' , czas trwania: ' + time + '(minut)';
+addWorkout.addEventListener('click', function() {
+	    excercise.innerHTML = "";
+	    excercise_set.map(function(item) {
+        var excercise1 = document.createElement('td');
+        excercise1.innerText = 'Trening: ' + item.name + ' , czas trwania: ' + item.time;
         excercise.appendChild(excercise1);
+        })
+        excercise_set.push('New excercise');
     });
 
 function startTimer(duration, display) {
-    var timer = duration, minutes;
+    var timer = duration, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, time);
-        seconds = parseInt(timer % 60, time);
+        minutes = parseInt(timer / 60,  workout_time);
+        seconds = parseInt(timer % 60,  workout_time);
 
         minutes = minutes < time ? "0" + minutes : minutes;
         seconds = seconds < time ? "0" + seconds : seconds;
 
         display.text(minutes + ":" + seconds);
 
-        if (--timer < 0) {
+        if (timer < workout_time) {
             timer = duration;
+            excercise.innerText = excercise_set[0];
         }
-    }, time);
-    var display = $('#time');
+        else {
+        	alert('Koniec!');
+        	excercise_set[0] = "";
+        }
+        
+    }, workout_time);
+    var display = $('#show_time');
 }
 
 
-var create = document.getElementById('create'),
+var create_workout_set = document.getElementById('create_workout_set'),
     set = document.getElementById('sort');
 
-create.addEventListener('click',function(){
-	if (excercise1.length = 0) {
+create_workout_set.addEventListener('click',function() {
+	if (excercise.length == 0) {
 		set.style.display = 'block';
 	}
 	else {
